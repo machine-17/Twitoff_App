ead911acc2860c77727c3cfd10a36115306709d5
Lester Gomez <68140323+Lolo-17@users.noreply.github.com>
2020-11-01 16:50:25 -0500

    testing

--- twitoff/app.py
+++ twitoff/app.py
@@ -86,6 +86,20 @@ def create_app():
 
     return app
 
+
+external_stylesheets = [
+    dbc.themes.SIMPLEX,
+    'https://use.fontawesome.com/releases/v5.9.0/css/all.css',
+]
+
+meta_tags=[
+    {'name': 'viewport', 'content': 'width=device-width, initial-scale=1'}
+]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
+app.config.suppress_callback_exceptions = True
+server = app.server
+
 footer = dbc.Container(
     dbc.Row(
         dbc.Col(
@@ -103,4 +117,8 @@ footer = dbc.Container(
     )
 )
 
-html.Div([footer])
+app.layout = html.Div([
+    dbc.Container(id='page-content', className='mt-4')
+    html.Hr(), 
+    footer
+])
